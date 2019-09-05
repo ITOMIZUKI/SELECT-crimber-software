5c59ccce4427fbec3af08035b78f732e781468bf
ITOMIZUKI <44338865+ITOMIZUKI@users.noreply.github.com>
2019-09-05 15:47:34 +0900

    Update messenger.py

--- messenger.py
+++ messenger.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 #IM920のデバッグ用プログラム
-import IM920
+import im920
 
 
 
 #ペアリング
-IM920.Srid(19200,'3AB3')
+#IM920.Srid(19200,'3AB3') #3F1C
 
 #削除
 #IM920.Erid(19200)
 
 #文字列送信
-IM920.Send(19200, "Hallo")
+#IM920.Send(19200, "Hallo")
 
 #文字列受信
-print(IM920.Reception(19200))
+#print(IM920.Reception(19200))
 
 #中継機化
 #IM920.Repeater(19200)
